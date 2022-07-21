639ff66f4f18bf910068de9efa3c177411fd5df1
Sergey <nsergeym@gmail.com>
2022-07-21 03:34:50 +0300

    Fixed

--- home_work_4/src/db/db.py
+++ home_work_4/src/db/db.py
@@ -1,18 +1,10 @@
-from databases import Database
-from sqlalchemy import MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlmodel import Session, create_engine
 from src.core.config import DATABASE_URL
 
-__all__ = ("get_session", 'SessionLocal', 'database')
+__all__ = ("get_session",)
 
 
 engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-metadata = MetaData()
-
-database = Database(DATABASE_URL)
 
 
 def get_session():
--- home_work_4/src/services/users.py
+++ home_work_4/src/services/users.py
@@ -1,60 +1,145 @@
-from typing import Optional, Union
+from functools import lru_cache
+from typing import Optional
 
-from sqlalchemy.orm import Session
+import jwt
+from fastapi import Depends
+from sqlmodel import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from src.api.v1.schemas.users import UserCreate, UserUpdate
-from src.core.security import get_password_hash, verify_password
+from src.api.v1.schemas.users import UserCreate, UserLogin, AboutUser, UserUpdate
+from src.core.security import blocked_access_tokens, settings, active_refresh_tokens
+from src.core.config import JWT_ALGORITHM
+from src.db import AbstractCache, get_cache, get_session
 from src.models.user import User
+from src.services import ServiceMixin
+
+__all__ = ("UserService", "get_user_service",)
 
 
-def get_user(db: Session, username: str) -> Optional[User]:
-    return db.query(User).filter(User.username == username).first()
+class UserService(ServiceMixin):
+    def create_user(self, user: UserCreate) -> dict:
+        new_user = User(
+            username=user.username,
+            password=generate_password_hash(user.password, method="sha256"),
+            email=user.email,
+        )
+        if self.session.query(User).filter(User.username == new_user.username).first():
+            return {"error": "User already exists."}
+        self.session.add(new_user)
+        self.session.commit()
+        self.session.refresh(new_user)
+        return {"msg": "User create.", "user": new_user}
+
+    def user_detail(self, _jwt) -> Optional[dict]:
+        _jwt.jwt_required()
+        current_user = _jwt.get_jwt_subject()
+        user_info = (
+            self.session.query(User).filter(User.username == current_user).first()
+        )
+        context = AboutUser(
+            uuid=user_info.uuid,
+            username=user_info.username,
+            password=user_info.password,
+            email=user_info.email,
+            created_at=user_info.created_at,
+            is_superuser=user_info.is_superuser,
+        )
+        return {"user": context}
+
+    def user_change(self, _jwt, user: UserUpdate):
+        _jwt.jwt_required()
+        current_username = _jwt.get_jwt_subject()
+        current_user = (
+            self.session.query(User).filter(User.username == current_username).first()
+        )
+        if self.session.query(User).filter(User.username == user.username).first():
+            return {"error": "User already exists."}
+        for key, value in user.dict().items():
+            setattr(current_user, key, value)
+        context = AboutUser(
+            uuid=current_user.uuid,
+            username=current_user.username,
+            password=current_user.password,
+            email=current_user.email,
+            created_at=current_user.created_at,
+            is_superuser=current_user.is_superuser,
+        )
+        self.session.add(current_user)
+        self.session.commit()
+        self.session.refresh(current_user)
+        jti = _jwt.get_raw_jwt()["jti"]
+        blocked_access_tokens.setex(jti, settings.access_expires, "true")
+        access_token = _jwt.create_access_token(subject=user.username)
+        return {"user": context, "access_token": access_token}
+
+    def login(self, _jwt, user: UserLogin) -> dict:
+        check_user = (
+            self.session.query(User).filter(User.username == user.username).first()
+        )
+        if not check_user:
+            return {"error": "User not found."}
+        if not check_password_hash(check_user.password, user.password):
+            return {"error": "Wrong password"}
+        user_uuid = (
+            self.session.query(User)
+            .filter(User.username == check_user.username)
+            .first()
+            .uuid
+        )
+        access_token = _jwt.create_access_token(subject=user.username)
+        refresh_token = _jwt.create_refresh_token(subject=user.username)
+        active_refresh_tokens.sadd(user_uuid, refresh_token)
+        return {"access_token": access_token, "refresh_token": refresh_token}
+
+    def logout(self, _jwt):
+        _jwt.jwt_refresh_token_required()
+        current_username = _jwt.get_jwt_subject()
+        user_uuid = (
+            self.session.query(User)
+            .filter(User.username == current_username)
+            .first()
+            .uuid
+        )
+        payload = _jwt.get_raw_jwt()
+        active_refresh_tokens.srem(user_uuid, get_token_from_payload(payload))
+        return {"msg": "User logout"}
+
+    def logout_all(self, _jwt):
+        _jwt.jwt_refresh_token_required()
+        current_username = _jwt.get_jwt_subject()
+        user_uuid = (
+            self.session.query(User)
+            .filter(User.username == current_username)
+            .first()
+            .uuid
+        )
+        active_refresh_tokens.delete(user_uuid)
+        return {"msg": "Logout all devices"}
+
+    def refresh(self, _jwt) -> dict:
+        _jwt.jwt_refresh_token_required()
+        current_username = _jwt.get_jwt_subject()
+        user_uuid = (
+            self.session.query(User)
+            .filter(User.username == current_username)
+            .first()
+            .uuid
+        )
+        payload = _jwt.get_raw_jwt()
+        new_access_token = _jwt.create_access_token(subject=current_username)
+        new_refresh_token = _jwt.create_refresh_token(subject=current_username)
+        active_refresh_tokens.sadd(user_uuid, new_refresh_token)
+        active_refresh_tokens.srem(user_uuid, get_token_from_payload(payload))
+        return {"access_token": new_access_token, "refresh_token": new_refresh_token}
 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+def get_token_from_payload(payload: dict):
+    return jwt.encode(payload, settings.authjwt_secret_key, JWT_ALGORITHM)
 
 
-def post(db: Session, payload: UserCreate):
-    db_user = User(username=payload.username,
-                   email=payload.email,
-                   password=get_password_hash(payload.password))
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-def get_all(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
-
-
-def get(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
-
-
-def authenticate_user(db: Session,
-                      username: str,
-                      password: str) -> Union[User, bool]:
-    user = get_user(db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.password):
-        return False
-    return user
-
-
-def put(db: Session, user_id: int, payload: UserUpdate):
-    db.query(User).filter(User.id == user_id).update({"username": payload.username,
-                                                      "email": payload.email,
-                                                      "password": get_password_hash(payload.password)})
-    db.commit()
-    return get(db, user_id)
-
-
-def delete(db: Session, user_id: int):
-    db_data = get(db, user_id)
-    db.delete(db_data)
-    db.commit()
-    return db_data
-
+@lru_cache()
+def get_user_service(
+    cache: AbstractCache = Depends(get_cache),
+    session: Session = Depends(get_session),
+) -> UserService:
+    return UserService(cache=cache, session=session)
